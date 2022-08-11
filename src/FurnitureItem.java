21b77fac96f7b090f5e6fc1fed5e467227c46eee
VIPUL NAVALE <vipulnavale9@gmail.com>
2022-08-11 12:10:38 +0530

    wrote method to calculate discounted ammount and method to display of furniture item

--- src/FurnitureItem.java
+++ src/FurnitureItem.java
@@ -17,7 +17,22 @@ public class FurnitureItem {
 
     }
 
+    public double discountedPrice(){
+        if(furnitureUsage.equals("OutDoor")) {
+            price = price * 1.05;
+        }
+        return price ;
+    }
 
+    public void display(){
+        System.out.println("Furniture code     = "+ furnitureCode);
+        System.out.println("Furniture type     = "+ furnitureType);
+        System.out.println("Grade of furniture = "+ gradeOfFurniture);
+        System.out.println("Color              = "+ color);
+        System.out.println("Usage of furniture = "+ furnitureUsage);
+        System.out.println("Price of furniture = "+ price);
+
+    }
 
 
 }
