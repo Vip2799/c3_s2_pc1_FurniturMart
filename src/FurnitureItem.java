edb38322e9b1234c312ee059b1e77fb6a833b97c
VIPUL NAVALE <vipulnavale9@gmail.com>
2022-08-11 12:58:50 +0530

    updated program with Scanner class

--- src/FurnitureItem.java
+++ src/FurnitureItem.java
@@ -2,35 +2,43 @@ public class FurnitureItem {
 
     int furnitureCode;
     String furnitureType;
-    char gradeOfFurniture;
+    String gradeOfFurniture;
     String color;
     String furnitureUsage;
     double price;
+    //    double priceOfChair = 780;
+//    double priceOfTable = 1430;
+//    double priceOfStool = 664;
+//    double priceOfCupboard = 2450;
+    double priceAfterDiscount;
 
-    FurnitureItem(){
-        furnitureCode = 401;
-        furnitureType = "Chair";
-        gradeOfFurniture = 'B';
-        color = "Blue";
-        furnitureUsage = "OutDoor";
-        price = 780 ;
+    FurnitureItem() {
+        furnitureCode = 0;
+        furnitureType = "";
+        gradeOfFurniture = "";
+        color = "";
+        furnitureUsage = "";
+        price = 0.0;
+        priceAfterDiscount = 0.0;
 
     }
 
     public void discountedPrice(){
         if(furnitureUsage.equals("OutDoor")) {
-            price = price * 0.95;
+            priceAfterDiscount = price * 0.95;
         }
-
     }
 
-    public void display(){
-        System.out.println("Furniture code     = "+ furnitureCode);
-        System.out.println("Furniture type     = "+ furnitureType);
-        System.out.println("Grade of furniture = "+ gradeOfFurniture);
-        System.out.println("Color              = "+ color);
-        System.out.println("Usage of furniture = "+ furnitureUsage);
-        System.out.println("Price of furniture = Rs "+ price);
+    public void display() {
+        System.out.println("Furniture code     = " + furnitureCode);
+        System.out.println("Furniture type     = " + furnitureType);
+        System.out.println("Grade of furniture = " + gradeOfFurniture);
+        System.out.println("Color              = " + color);
+        System.out.println("Usage of furniture = " + furnitureUsage);
+        System.out.println("Price of furniture = Rs " + price);
+        if (furnitureUsage.equals("OutDoor")) {
+            System.out.println("Price of furniture after discount of 5 % = " + priceAfterDiscount);
+        }
 
     }
 
--- src/FurnitureItemImpl.java
+++ src/FurnitureItemImpl.java
@@ -1,7 +1,23 @@
+import java.util.Scanner;
 public class FurnitureItemImpl {
     public static void main(String[] args) {
-        FurnitureItem chair = new FurnitureItem();
-        chair.discountedPrice();
-        chair.display();
+        Scanner sc = new Scanner(System.in);
+        FurnitureItem item1 = new FurnitureItem();
+        System.out.println("Enter the furniture code");
+        item1.furnitureCode = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Enter the furniture type");
+        item1.furnitureType = sc.nextLine();
+        System.out.println("Enter Grade of Furniture");
+        item1.gradeOfFurniture = sc.nextLine();
+        System.out.println("Enter the color ");
+        item1.color = sc.next();
+        System.out.println("Usage of furniture ");
+        item1.furnitureUsage = sc.next();
+        System.out.println("Enter price of furniture ");
+        item1.price = sc.nextDouble();
+
+        item1.discountedPrice();
+        item1.display();
     }
 }
